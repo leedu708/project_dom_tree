06ed1c4225100e48614ee7366f7d5c8a9cb5b440
leedu708 <leedu708@gmail.com>
2015-09-11 02:03:38 -0400

    added comments to builder

--- dom_builder.rb
+++ dom_builder.rb
@@ -1,5 +1,4 @@
 class DOMBuilder
-  attr_reader :write_array
 
   def initialize(tree)
 
@@ -10,24 +9,30 @@ class DOMBuilder
 
   end
 
+  # need to configure tab structure
   def build(node)
 
+    # add <node.name to write_array
     @write_array << "<#{node.name}"
 
+    # if node has classes, append them to last element of write array
     unless node.classes == nil
       @write_array[-1] = @write_array[-1] + " classes=\"#{node.classes.join(" ")}\""
     end
 
+    # if node has id's, append them to last element of write array.  else append closing bracket
     if node.id == nil
       @write_array[-1] = @write_array[-1] + ">\n"
     else
       @write_array[-1] = @write_array[-1] + " id=\"#{node.id}\">\n"
     end
 
+    # add tag text to array if available
     unless node.text == ""
       @write_array << node.text + "\n"
     end
 
+    # build children
     unless node.children == []
       node.children.each do |child|
         build(child)
@@ -37,10 +42,12 @@ class DOMBuilder
       return
     end
 
+    # add closing tag e.g. </html>
     @write_array << "</#{node.name}>\n"
 
   end
 
+  # prints write_array to file
   def write_file
 
     File.open("rebuild.html", "w") do |file|
